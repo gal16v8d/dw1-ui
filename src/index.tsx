1543ad2fb63477858d141fdb2297b0b2a4957de9
alexg <alex.galvis.sistemas@gmail.com>
2023-03-17 22:29:21 -0500

    Fix enable mocks flag

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './config/i18n/languageConfig';
 
-if (process.env.REACT_APP_ENABLE_MOCKS) {
+if (process.env.REACT_APP_ENABLE_MOCKS === 'true') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { worker } = require('./mocks/browser');
   worker.start();

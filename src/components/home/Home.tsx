48ffcbccc17d55766da9fd8998aa30fd04002e11
alexg <alex.galvis.sistemas@gmail.com>
2023-03-17 19:53:34 -0500

    Replace images and customize a little bit more home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,19 @@
 import { useListingContext } from 'provider/listing/Dw1ListingProvider';
+import { Image } from 'primereact/image';
 
 const Home = (): JSX.Element => {
   const { t } = useListingContext();
   return (
-    <>
-      <div style={{ background: 'white' }}>{t('home.text')}</div>
-    </>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <header className="header">
+        <h3 className="h3" style={{ color: 'white' }}>
+          {t('home.text')}
+        </h3>
+      </header>
+      <Image src="/assets/img/dw1_ps1.png" alt="Dw1 Image" width="250" />
+    </div>
   );
 };
 
